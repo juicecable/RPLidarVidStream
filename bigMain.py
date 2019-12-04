1e3a6c4c597b674f0deb35eef5703b24280d661b
Derek Frombach <neoredstone@gmail.com>
2019-12-04 12:08:16 -0600

    Update bigMain.py
    Fixed Memory Leak, Included Locks, Handled Access Issue Bug to retry frame then drop frame.
    
    Access issue bug still exists, this is a bug in python.

--- bigMain.py
+++ bigMain.py
@@ -11,12 +11,13 @@ import time
 import math
 import socket
 import multiprocessing
-from multiprocessing import shared_memory
+from multiprocessing import shared_memory, Lock
 
 ip='0.0.0.0' #Don't Change This
 port=8081 #Hosting Port, Don't Change This
 buff=1500 #Also Don't Change This
 port_name='/dev/ttyUSB0' #Also Don't Change This
+debug=False
 
 #Initalisation of TCP Socket Server
 s=socket.socket(socket.AF_INET, socket.SOCK_STREAM) #TCP/IP Socket
@@ -55,51 +56,76 @@ addr=["NC","NC"]
 #Definitions
 
 #Core Reciever
-def get_data(smq,sma,smd,sml,sms,port_name):
+def get_data(smq,sma,smd,sml,sms,l,port_name):
     lidar=RPLidar(None,port_name)
     lis=lidar.iter_scans
     for scan in lis():
         if sms[0]: break
+        l.acquire() #Locking
         sml[0]=int(len(scan))
         for x in range(0,sml[0]):
             n=scan[x]
             smq[x]=n[0]
             sma[x]=n[1]
             smd[x]=n[2]
+        l.release() #Unlocking
     lidar.stop()
     lidar.set_pwm(0)
     lidar.disconnect()
     print('SCAN STOPPED!')
 
 #Renderer
-def process_data(smq,sma,smd,sml,dp):
+def process_data(smq,sma,smd,sml,calls,dp):
     #Quality, Angle, Distance
     #Quality is 0-15
     #Angle is 0.0-360.0
     #Distance is 0.0-12000.0
-    mdist=max(smd) #Auto-Scaling
-    if mdist>0:
-        if sml[0]>360:
-            q=(360/sml[0])*(360/(2*pi))
-        else:
-            q=360/(2*pi)
-        for x in range(0,sml[0]):
-            dist=(smd[x]/mdist)*255 #Auto-Scale
-            w=mf((mc(sma[x]/q)*dist)+256)
-            h=mf((ms(sma[x]/q)*dist)+256)
-            g=mf((smq[x])*17)
-            r=255-g
-            dp((w,h),(r,g,255))
+    if calls<2:
+        try:
+            mdist=max(smd) #Auto-Scaling
+            if mdist>0:
+                if sml[0]>360:
+                    q=(360/sml[0])*(360/(2*pi)) #Auto-Adapt
+                else:
+                    q=360/(2*pi) #Auto-Adapt
+                for x in range(0,sml[0]):
+                    dist=(smd[x]/mdist)*255 #Auto-Scale
+                    w=mf((mc(sma[x]/q)*dist)+256)
+                    h=mf((ms(sma[x]/q)*dist)+256)
+                    v=mf((smq[x])*17)
+                    dp((w,h),(v,v,v)) #drawing
+        except ValueError: #For Some Stupid Error Where Shared Memory Lists Decide that they have a Length of Zero for only one line
+            #Retry Frame Once
+            process_data(smq,sma,smd,sml,calls+1,dp)
+            #print('Caught Exception')
+            #print('Dist')
+            #print(smd)
+            #print('Angle')
+            #print(sma)
+            #print('Quality')
+            #print(smq)
+            #print('Len')
+            #print(sml)
+            #print('FIN')
+            #print(len(smd))
+            #print(len(sma))
+            #print(len(smq))
+            #print(len(sml))
+            #print('Exception Handled')
+    else:
+        if debug: print('Dropped Frame')
+        pass
         
 #Now the Actual Code
 
 #Run Before Connect
+l=Lock()
 smq=shared_memory.ShareableList([0.0]*400)
 sma=shared_memory.ShareableList([0.0]*400)
 smd=shared_memory.ShareableList([0.0]*400)
 sml=shared_memory.ShareableList([0])
 sms=shared_memory.ShareableList([False])
-p1=multiprocessing.Process(target=get_data,args=(smq,sma,smd,sml,sms,port_name),daemon=True)
+p1=multiprocessing.Process(target=get_data,args=(smq,sma,smd,sml,sms,l,port_name),daemon=True)
 p1.start()
 
 #Continuity Loop
@@ -181,7 +207,7 @@ while True:
         dp((255,255),(0,0,255)) #RPLidar
                         
         #Data Processing with Concurrent Updating
-        process_data(smq,sma,smd,sml,dp)
+        process_data(smq,sma,smd,sml,1,dp)
         
         #Converting Raw Image into Compressed JPEG Bytes
         with bio() as output:
@@ -214,15 +240,17 @@ while True:
 f.close()
 s.close()
 #kill lidar
+l.acquire()
 sms[0]=True
-ts(1.0)
-smq.close()
-smq.unlink()
-sma.close()
-sma.unlink()
-smd.close()
-smd.unlink()
-sml.close()
-sml.unlink()
-sms.close()
-sms.unlink()
+l.release()
+p1.join() #Wait for Process to Finish
+smq.shm.close()
+smq.shm.unlink()
+sma.shm.close()
+sma.shm.unlink()
+smd.shm.close()
+smd.shm.unlink()
+sml.shm.close()
+sml.shm.unlink()
+sms.shm.close()
+sms.shm.unlink()
